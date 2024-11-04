a70ea4eea854d9fc6244119e585b7f7d8e8e12f5
RamiKrispin <ramkrisp@umich.edu>
2024-11-03 19:12:40 -0800

    add CPU settings

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -4,20 +4,34 @@
 user_name=rkrispin
 image_label=pydata_ny_workshop
 image_tag=0.0.3
-image_name=$user_name/$image_label:$image_tag
 venv_name="pydata-ny-workshop"
+
+
+# Identify the CPU type (M1 vs Intel)
+if [[ $(uname -m) ==  "aarch64" ]] ; then
+    CPU="arm64"
+elif [[ $(uname -m) ==  "arm64" ]] ; then
+    CPU="arm64"
+else
+    CPU="amd64"
+fi
+
+tag="$CPU.$image_tag"
+image_name="rkrispin/$image_label:$tag"
+
+
 
 echo "Build the docker"
 
 docker build . -f Dockerfile \
-               --progress=plain \
-               --build-arg QUARTO_VER=$QUARTO_VER \
-               --build-arg VENV_NAME=$venv_name \
-               -t $image_name
+                --progress=plain \
+                --build-arg QUARTO_VER=$QUARTO_VER \
+                --build-arg VENV_NAME=$venv_name \
+                -t $image_name
 
 if [[ $? = 0 ]] ; then
 echo "Pushing docker..."
-#docker push $image_name
+docker push $image_name
 else
 echo "Docker build failed"
 fi
